6cadb2a67bd63acfe6b546379d72fc5b24a68288
Mohammad Umar <mohammadumar8010@gmail.com>
2026-05-06 23:51:27 +0530

    fixed navbar

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
-import Navbar from "../components/Navbar";
+import Navbar from "./components/Navbar";
+
 
 export default function HomePage() {
   return (
     <main>
-      <Navbar />
+      <Navbar/>
+      
 
       <section className="h-screen flex items-center justify-center">
         <h1 className="text-6xl font-bold">
