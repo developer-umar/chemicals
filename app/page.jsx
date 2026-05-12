f87368caae4af03ee27ceebef1aa5f57e512cdb8
Mohammad Umar <mohammadumar8010@gmail.com>
2026-05-12 15:44:07 +0530

    updated page

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,13 @@
 import AboutSection from "./components/AboutSection";
-// import GallerySection from "./components/GallerySection";
+import GallerySection from "./components/GallerySection";
 import CoreBusiness from "./components/CoreBusiness";
-// import GlobalNetwork from "./components/GlobalNetwork";
-// import IndustriesSection from "./components/IndustriesSection";
+import GlobalNetwork from "./components/GlobalNetwork";
+import IndustriesSection from "./components/IndustriesSection";
 import Navbar from "./components/Navbar";
-// import ProductCategories from "./components/ProductCategories";
-// import TeamSection from "./components/TeamSection";
-// import WhyChooseUs from "./components/WhyChooseUs";
-// import ClientShowcase from "./components/ClientShowcase";
-// import ParallaxCTA from "./components/ParallaxCTA";
+import TeamSection from "./components/TeamSection";
+import WhyChooseUs from "./components/WhyChooseUs";
+import ClientShowcase from "./components/ClientShowcase";
+import ParallaxCTA from "./components/ParallaxCTA";
 import Footer from "./components/Footer";
 import HeroSection from "./components/Hero";
 export const metadata = {
@@ -37,14 +36,14 @@ export default function HomePage() {
       
       <AboutSection/>
       <CoreBusiness/>
-      {/* <ProductCategories/>
+    
       <WhyChooseUs/>
       <GlobalNetwork/>
       <TeamSection/>
       <IndustriesSection/>
       <GallerySection/>
       <ClientShowcase/>
-      <ParallaxCTA/> */}
+      <ParallaxCTA/> 
       <Footer/>
       
     </main>
