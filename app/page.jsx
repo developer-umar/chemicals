1885ec6a7a4d683c2bc517913ce883cbdd1636a7
Mohammad Umar <mohammadumar8010@gmail.com>
2026-05-12 14:59:33 +0530

    updated page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,14 @@
 import AboutSection from "./components/AboutSection";
-import GallerySection from "./components/GallerySection";
+// import GallerySection from "./components/GallerySection";
 import CoreBusiness from "./components/CoreBusiness";
-import GlobalNetwork from "./components/GlobalNetwork";
-import IndustriesSection from "./components/IndustriesSection";
+// import GlobalNetwork from "./components/GlobalNetwork";
+// import IndustriesSection from "./components/IndustriesSection";
 import Navbar from "./components/Navbar";
-import ProductCategories from "./components/ProductCategories";
-import TeamSection from "./components/TeamSection";
-import WhyChooseUs from "./components/WhyChooseUs";
-import ClientShowcase from "./components/ClientShowcase";
-import ParallaxCTA from "./components/ParallaxCTA";
+// import ProductCategories from "./components/ProductCategories";
+// import TeamSection from "./components/TeamSection";
+// import WhyChooseUs from "./components/WhyChooseUs";
+// import ClientShowcase from "./components/ClientShowcase";
+// import ParallaxCTA from "./components/ParallaxCTA";
 import Footer from "./components/Footer";
 import HeroSection from "./components/Hero";
 export const metadata = {
